a0b89df1f47425110a053f4e983447d8b34a0d4f
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2021-03-03 13:05:38 +0700

    adding

---
+++ app.js
@@ -0,0 +1,13 @@
+let testing = () => {
+  return "OK";
+};
+
+let bio = (nama) => {
+  return `hello ${nama}`;
+};
+
+let nambah = (angka1, angka2) => {
+  return angka1 + angka2;
+};
+
+module.exports = { testing, bio, nambah };
